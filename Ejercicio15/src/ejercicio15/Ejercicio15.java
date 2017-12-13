495dc59fd5a2877fc7b6ef9a90843da39911310d
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-12-13 09:57:49 +0100

    Programa terminado

---
+++ Ejercicio15/src/ejercicio15/Ejercicio15.java
@@ -0,0 +1,17 @@
+package ejercicio15;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalcularNota calc=new CalcularNota();
+        calc.calcularNotaFin();
+    }
+
+}
